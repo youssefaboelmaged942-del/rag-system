2e5374199abfc6d30a05d447a24dd184dc56c580
Youssef Abo El-Magd <youssefaboelmaged942@gmail.com>
2026-08-02 14:09:31 +0300

    Update 06_retrieve_context.py

--- 06_retrieve_context.py
+++ 06_retrieve_context.py
@@ -7,7 +7,7 @@ CHROMA_DIR = _store.CHROMA_DIR
 _vec = import_module("04_vector_representation")
 embed_texts = _vec.embed_texts
 
-DEFAULT_DISTANCE_THRESHOLD = 0.70 
+DEFAULT_DISTANCE_THRESHOLD = 0.40
 
 
 def retrieve_context(
